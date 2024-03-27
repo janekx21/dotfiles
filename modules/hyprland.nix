de366514c7185efc1205173d8a641d293e089475
Janek Winkler <janekx21@gmail.com>
2024-03-27 14:28:26 +0100

    Wed Mar 27 02:28:26 PM CET 2024

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -36,6 +36,26 @@ in
 
 	programs.rofi.package = pkgs.rofi-wayland;
 
+	gtk = {
+    enable = true;
+    theme = {
+      name = "Catppuccin-Macchiato-Compact-Pink-Dark";
+      package = pkgs.catppuccin-gtk.override {
+        accents = [ "pink" ];
+        size = "compact";
+        tweaks = [ "rimless" "black" ];
+        variant = "macchiato";
+      };
+    };
+  };
+
+	# Now symlink the `~/.config/gtk-4.0/` folder declaratively:
+	xdg.configFile = {
+	  "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
+	  "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
+	  "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+	};
+
   wayland.windowManager.hyprland = {
     enable = true;
     package = lib.mkForce (lib.makeOverridable ({enableXWayland, enableNvidiaPatches}: wrappWithNixGL pkgs pkgs.hyprland) {enableXWayland = true; enableNvidiaPatches = false;});
