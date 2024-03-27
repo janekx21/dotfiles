12c9589f19b7c3fd094d1e71b4507a0c9b04ab2c
Janek Winkler <janekx21@gmail.com>
2024-03-27 14:19:06 +0100

    Wed Mar 27 02:19:06 PM CET 2024

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -128,7 +128,7 @@ in
         "$mod, E, exec, ${pkgs.gnome.nautilus}/bin/nautilus -w"
         # "$mod, X, exec, ${pkgs.rambox}/bin/rambox --no-sandbox"
 				"$mod, u, exec, ${config.programs.kitty.package}/bin/kitty ~/Git/dotfiles/change.bash"
-        "$mod, R, exec, ${config.programs.rofi.package}/bin/rofi -show drun"
+        "$mod, R, exec, ${config.programs.rofi-wayland.package}/bin/rofi -show drun"
 
 				# System shortcuts
         "$mod, C, killactive,"
