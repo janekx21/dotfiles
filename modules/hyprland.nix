3d617e6653b9826ed0d7ee8d99fb17e799435a60
Janek Winkler <janekx21@gmail.com>
2024-04-01 02:01:16 +0200

    Mon Apr  1 02:01:16 AM CEST 2024

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -94,7 +94,7 @@ in
 		    sensitivity = 0.1; # -1.0 - 1.0, 0 means no modification.
 		    accel_profile = "flat";
       };
-			# gestures.workspace_swip = true;
+			gestures.workspace_swipe = true;
 
 			# Style stuff
       general = {
