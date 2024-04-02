448e3e08cff1601335b648adabb1b16d565376be
Janek Winkler <janek.winkler@bridgefield.de>
2024-04-02 14:44:16 +0200

    Di 2. Apr 14:44:16 CEST 2024

--- home/janek@torpedo.nix
+++ home/janek@torpedo.nix
@@ -31,7 +31,7 @@ in
 
   wayland.windowManager.hyprland.settings = {
     monitor = [
-      "eDP-1,preferred,auto,1.4"
+      "eDP-1,preferred,auto,1.333333"
       ",preferred,auto,auto"
     ];
   };
