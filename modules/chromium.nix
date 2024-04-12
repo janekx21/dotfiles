d41fbaeb28ca4bf64a8bb00263f6628f218a4522
Janek Winkler <janekx21@gmail.com>
2024-04-12 14:36:20 +0200

    Fri Apr 12 02:36:20 PM CEST 2024

--- modules/chromium.nix
+++ modules/chromium.nix
@@ -2,17 +2,22 @@
 
 let
   wrappWithNixGL = import ../utils/wrapp-with-nix-gl.nix;
+  commandLineArgs = [
+    "--enable-features=TouchpadOverscrollHistoryNavigation"
+    "--enable-logging=stderr"
+    "--ignore-gpu-blocklist"      
+  ];
 in
 {
   programs.chromium = {
     enable = true;
     # package = wrappWithNixGL pkgs pkgs.ungoogled-chromium;
-    package = lib.mkForce (lib.makeOverridable ({commandLineArgs}: wrappWithNixGL {inherit pkgs; pkg = pkgs.ungoogled-chromium;}) {commadLineArgs = config.programs.chromium.commandLineArgs; });
-    commandLineArgs = [
-      "--enable-features=TouchpadOverscrollHistoryNavigation"
-      "--enable-logging=stderr"
-      "--ignore-gpu-blocklist"      
-    ];
+    package = wrappWithNixGL {inherit pkgs; pkg = pkgs.ungoogled-chromium;args = lib.concatStringsSep " " commandLineArgs;};
+    # commandLineArgs = [
+    #   "--enable-features=TouchpadOverscrollHistoryNavigation"
+    #   "--enable-logging=stderr"
+    #   "--ignore-gpu-blocklist"      
+    # ];
     # programs.chromium.extensions
 # [
 #   { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; } # ublock origin
