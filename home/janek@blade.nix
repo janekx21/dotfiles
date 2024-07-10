aca3e375d7971b66cac39525a7051ae033267ebd
Janek Winkler <janekx21@gmail.com>
2024-07-10 14:44:33 +0200

    Wed Jul 10 02:44:33 PM CEST 2024

--- home/janek@blade.nix
+++ home/janek@blade.nix
@@ -53,7 +53,7 @@
       name = "Tela-dark";
       package = pkgs.tela-icon-theme;
     };
-  };
+  };pkgs.gnome.adwaita-icon-theme
 	xdg.configFile = {
 	  "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
 	  "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
