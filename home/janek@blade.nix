4d4d73d7afe3fc28070cb41e46cb41ee3e5c093f
Janek Winkler <janekx21@gmail.com>
2024-10-01 20:06:00 +0200

    Tue Oct  1 20:06:00 CEST 2024

--- home/janek@blade.nix
+++ home/janek@blade.nix
@@ -469,7 +469,7 @@
           plugin = tmuxPlugins.continuum;
           extraConfig = ''
             set -g @continuum-restore 'on'
-            set -g @continuum-save-interval '60' # minutes
+            set -g @continuum-save-interval '10' # minutes
           '';
         }
       ];
