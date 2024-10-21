e92350fb247327a908c89c3f2365ef66f9323f35
Janek Winkler <janek.winkler@bridgefield.de>
2024-10-21 19:52:26 +0200

    Mo 21. Okt 19:52:26 CEST 2024

--- modules/kitty.nix
+++ modules/kitty.nix
@@ -1,17 +1,17 @@
 { pkgs, lib, ... }:
 
 let
-  wrappWithNixGL = import ../utils/wrapp-with-nix-gl.nix;
+  # wrappWithNixGL = import ../utils/wrapp-with-nix-gl.nix;
 in
 {
   programs.kitty = {
-    enable = true;
+    # enable = true;
     font = {
       size = 12;
       name = "JetBrainsMono Nerd Font"; # TODO use nix pkgs
     };
     shellIntegration.enableZshIntegration = true;
-    package = wrappWithNixGL {inherit pkgs; pkg = pkgs.kitty;};
+    # package = wrappWithNixGL {inherit pkgs; pkg = pkgs.kitty;};
     theme = "Gruvbox Dark Hard";
     settings = {
       background_opacity = "0.90";
