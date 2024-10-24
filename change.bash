74a527ea267db3f7d94938fed2bc52659b5f72a1
Janek Winkler <janekx21@gmail.com>
2024-10-24 16:08:27 +0200

    Thu Oct 24 16:08:27 CEST 2024

--- change.bash
+++ change.bash
@@ -7,6 +7,6 @@ cd "$(dirname "$0")"
 git pull || echo "git pull failed"
 hx ./flake.nix
 git add .
-git commit -m "$(date)"
-home-manager --flake .#$(whoami)@$(hostname -s) switch -b backup-$RANDOM -v
+git commit -m "$(date)" || echo "git commit failed"
+home-manager --flake .#$(whoami)@$(hostname -s) switch -b backup-$RANDOM --show-trace
 git push || echo "git push failed"
