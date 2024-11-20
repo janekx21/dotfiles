22e510982419d09bb05a889b2fbb0e7061e2fc48
Janek Winkler <janekx21@gmail.com>
2024-11-20 15:43:07 +0100

    Wed Nov 20 03:43:07 PM CET 2024

--- modules/kitty.nix
+++ modules/kitty.nix
@@ -14,7 +14,7 @@ in
     package = wrappWithNixGL {inherit pkgs; pkg = pkgs.kitty;};
     theme = "Gruvbox Dark Hard";
     settings = {
-      background_opacity = if config.wayland.windowManager.hyprland.enabled then "0.90" else "1.00"; 
+      background_opacity = if config.wayland.windowManager.hyprland.enable then "0.90" else "1.00"; 
       # background_opacity = "0.90";
       background_blur = "16";
     };
